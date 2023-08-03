3b4daf2a60e55dee30620c5b4a9a0afd59839dd2
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-08-03 13:41:49 -0400

    Added .env to stop opening a new browser every time

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,7 +3,7 @@ function Header(propsObj) {
         <header className="row justify-between align-center">
             <h3>Logo</h3>
             <nav>
-                <a className="active" href="#">Home</a>
+                <a className={propsObj.name === "landing" ? "active" : ""} href="#">Home</a>
                 <a className="active" href="#">About</a>
                 <a className="active" href="#">Contact</a>
             </nav>
