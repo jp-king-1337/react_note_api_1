7980611d474a57a7bee065fe68b5db6375e3de4c
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-08-04 10:53:49 -0400

    Updated

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,11 +1,12 @@
 function Landing() {
-    const students = ["Erin", "Hernan", "Liam", "Jonathan"];
+    const [students, setStudents] = useState(["Erin", "Hernan", "Liam", "Jonathan"]);
 
     return (
         <main>
             <h1>Landing</h1>
             <p>Welcome to our React Starter Site</p>
 
+            <h3>Student List:</h3>
             {students.map((student, i) => (
                 <p key={i}>{student}</p>
             ))}
