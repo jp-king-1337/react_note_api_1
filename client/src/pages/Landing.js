726c60b038b094e69140371476add250e2a90f55
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-08-04 11:27:08 -0400

    Removed line 16. Cool functionality, showing the text entered in the input box as a paragraph, but it's not necessary right now

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -13,8 +13,6 @@ function Landing() {
             <h1>Landing</h1>
             <p>Welcome to our React Starter Site</p>
 
-            <p style={{ background: studentName }}>{studentName}</p>
-
             <h3>Student List:</h3>
             {students.map((student, i) => (
                 <p key={i}>{student}</p>
