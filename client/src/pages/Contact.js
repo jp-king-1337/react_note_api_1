d51b0ec4c975b744b42be1d6f057d4ef51705a92
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-08-04 13:02:08 -0400

    Styled and added some more after lunch

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,7 +1,29 @@
+import React, { useState } from "react";
+
 function Contact() {
+    // const [userName, setUserName] = useState("");
+    // const [email, setEmail] = useState("");
+    // const [message, setMessage] = useState("");
+    const [formData, setFormData] = useState({
+        userName: "",
+        email: "",
+        message: ""
+    });
+
+    const handleInputChange = event => {
+        console.log(event.target.value);
+    };
+
     return (
         <main>
-            <h1>Contact</h1>
+            <h1 className="text-center">Contact Me</h1>
+
+            <form className="column">
+                <input onChange={handleInputChange} value={formData.userName} type="text" placeholder="Enter name" />
+                <input onChange={handleInputChange} type="email" placeholder="Enter email address" />
+                <textarea onChange={handleInputChange} cols="30" rows="10" placeholder="Enter your message" />
+                <button>Submit</button>
+            </form>
         </main>
     )
 }
