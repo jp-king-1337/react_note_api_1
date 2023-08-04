924b9dae4f940ce053045da3c46d70ed7b577378
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-08-04 13:18:15 -0400

    Update

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -13,16 +13,40 @@ function Contact() {
     const handleInputChange = event => {
         const stateProp = event.target.name;
         const value = event.target.value;
+
+        setFormData({
+            ...formData,
+            [stateProp]: value
+        });
     };
 
     return (
         <main>
             <h1 className="text-center">Contact Me</h1>
 
+            {/* An example to assist understanding of how handleInputChange() works */}
+            <p>userName: {formData.userName}</p>
+            <p>email: {formData.email}</p>
+            <p>message: {formData.message}</p>
+
             <form className="column">
-                <input name="userName" onChange={handleInputChange} value={formData.userName} type="text" placeholder="Enter name" />
-                <input onChange={handleInputChange} type="email" placeholder="Enter email address" />
-                <textarea onChange={handleInputChange} cols="30" rows="10" placeholder="Enter your message" />
+                <input
+                    name="userName"
+                    onChange={handleInputChange}
+                    value={formData.userName}
+                    type="text"
+                    placeholder="Enter name" />
+                <input
+                    name="email"
+                    onChange={handleInputChange}
+                    type="email"
+                    placeholder="Enter email address" />
+                <textarea
+                    name="message"
+                    onChange={handleInputChange}
+                    cols="30"
+                    rows="10"
+                    placeholder="Enter your message" />
                 <button>Submit</button>
             </form>
         </main>
