b5c0f0f28d132bf4ba75f4340299bf3ed85f137a
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-08-07 12:25:17 -0400

    Something went wrong while following along

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import { getNames } from "./utils";
 
 // Component Imports
 import Header from "./components/Header";
@@ -8,13 +10,11 @@ import Footer from "./components/Footer";
 import Landing from "./pages/Landing";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import { getNames } from "./utils";
 
 // Configuring .env file
 // dotenv.config();
 
 function App() {
-    const [page, setPage] = useState("landing");
     const [studentName, setStudentName] = useState("");
     const [students, setStudents] = useState([]);
     const [darkMode, setDarkMode] = useState(false);
@@ -23,36 +23,28 @@ function App() {
         const names = getNames();
 
         setStudents([...names]);
-        console.log("Use Effect");
     }, []);
-
-    const handlePageView = () => {
-        switch (page) {
-            case "landing":
-                return <Landing
-                    studentName={studentName}
-                    setStudentName={setStudentName}
-                    students={students}
-                    setStudents={setStudents}
-                />;
-            case "about":
-                return <About />;
-            default:
-                return <Contact />;
-        }
-    }
 
     return (
         <div className={`container ${darkMode ? "dark" : ""}`}>
             <Header
                 darkMode={darkMode}
-                setDarkMode={setDarkMode}
-                page={page}
-                setPage={setPage} />
+                setDarkMode={setDarkMode} />
 
-            {handlePageView()}
+            <Routes>
+                <Route path="/" element={(
+                    <Landing
+                        setStudentName={setStudentName}
+                        setStudents={setStudents}
+                        students={students} />
+                )} />
 
-            <Footer />
+                <Route path="/about" element={<About />} />
+
+                <Route path="/contact" element={<Contact />} />
+            </Routes>
+
+            <Footer studentName={studentName} />
         </div>
     );
 }
